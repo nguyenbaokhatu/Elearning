ea7425a686f75a60e98e5454ba19a3fb1d636a47
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-03-22 14:53:59 +0700

    show and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,37 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def logout
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def current_user
+    if(user_id = session[:user_id])
+      @current_user ||= User.find(user_id)
+    elsif(user_id = cookies.signed[:user_id])
+      user = User.find(user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
