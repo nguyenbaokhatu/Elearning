c11cabcd0e21fee7182dfd8577c5f1a202e1dc99
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-03-22 12:37:48 +0700

    sign in and remember me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      login @user
+      remember @user
       flash[:success] = "Register success"
       redirect_to root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'users/sign_up' => 'users#new'
   post 'users/sign_up' => 'users#create'
+  get 'users/sign_in' => 'sessions#new'
+  post 'users/sign_in' => 'sessions#create'
 end
