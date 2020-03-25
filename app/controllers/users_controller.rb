7676edb2a33126ae42cd0808ec37e3bcdc0d1993
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-03-25 20:16:24 +0700

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      UserMailer.with(user: self).welcome_email.deliver_now
+      UserMailer.with(user: @user).welcome_email.deliver_now
       sign_in @user
       remember @user
       flash[:success] = "Register success"
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url  = sign_in_url
+    @url  = users_sign_in_url
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
