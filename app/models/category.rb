b96672fea45257efd85232f09c9218a36aa2d014
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-03-29 16:13:09 +0700

    Word Learner

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,8 +3,6 @@
 class Category < ApplicationRecord
   has_many :words
   has_many :lessons, dependent: :destroy
-  has_many :users, through: :lessons
-  has_many :lesson_words, through: :lessons
 
   validates :name, presence: true
 end
