80a819da2c446a4a5a767458ebb107f1a1000988
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-03-29 19:46:56 +0700

    association

--- app/models/word_answer.rb
+++ app/models/word_answer.rb
@@ -2,7 +2,7 @@
 
 class WordAnswer < ApplicationRecord
   belongs_to :word
-  has_many :lessons, through: :lesson_word
+  has_many :lessons, through: :lesson_words
 
   validates :content, presence: true, length: { maximum: 20 }
 end
