0951a3813b98633d9e187012f8c47cc743fd050b
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-04-01 20:03:18 +0700

    init

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'nguyenbaokhatu',
+  :password => 'Khatu2013',
+  :domain => 'infinite-brushlands-18662.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
