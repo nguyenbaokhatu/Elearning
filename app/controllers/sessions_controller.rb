4cae311dfac5d381cc0a259ec675718f00e75b62
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-04-03 09:10:06 +0700

    association

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
       flash[:success] = 'login success'
-      sign_in user
+      login user
       redirect_to user
     else
       flash.now[:danger] = 'In Invalid email/password combination'
