9a612ad3879b8809d64a8276af2335ade9610e3b
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-04-03 09:06:45 +0700

    association

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,6 @@ class User < ApplicationRecord
 
   def authenticated?(remember_token)
     return false if remember_digest.nil?
-
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
